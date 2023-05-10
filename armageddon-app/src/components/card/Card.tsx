4043cac1e3b626be8d57adeac32ed7642ef999e4
kn0wleadge <firsovev@mstu.edu.ru>
2023-05-10 23:49:12 +0300

    task11

--- armageddon-app/src/components/card/Card.tsx
+++ armageddon-app/src/components/card/Card.tsx
@@ -2,28 +2,31 @@ import "./Card.css"
 import {CardImage} from "./cardImage/CardImage";
 import {CardAction} from "./cardAction/CardAction";
 import {CardContent} from "./cardContent/CardContent";
+
 type AsteroidCardProps = {
-    name:string;
-    date:string;
-    distance:number;
-    size:number;
-    isDangerous:boolean;
-    displayTip:boolean
+    name: string;
+    date: string;
+    distance: number;
+    size: number;
+    isDangerous: boolean;
+    displayTip: boolean
 }
 export const Card = (props: AsteroidCardProps) => {
-    const {name,date,distance,size,isDangerous,displayTip}=props
+    const {name, date, distance, size, isDangerous, displayTip} = props
     return <div className={"container"}>
-        <div className={`${isDangerous ? `CardNotOK` : `CardOK`}`}><div className={`card-${sizeType(size)}`}>
-            <CardImage/>
-            <CardContent name={name} date={date} distance={distance} size={size} displayTip={displayTip}/>
-            <CardAction isDangerous={isDangerous}/>
-        </div></div>
+        <div className={`${isDangerous ? `CardNotOK` : `CardOK`}`}>
+            <div className={`card-${sizeType(size)}`}>
+                <CardImage/>
+                <CardContent name={name} date={date} distance={distance} size={size} displayTip={displayTip}/>
+                <CardAction isDangerous={isDangerous}/>
+            </div>
+        </div>
     </div>
 }
 const sizeType = (s) => {
-    if (s>700)
+    if (s > 700)
         return 3
-    else if (s>300)
+    else if (s > 300)
         return 2
     else
         return 1
--- armageddon-app/src/components/card/cardContent/CardContent.tsx
+++ armageddon-app/src/components/card/cardContent/CardContent.tsx
@@ -1,24 +1,25 @@
 import "./CardContent.css"
+
 type AsteroidCardProps = {
-    name:string;
-    date:string;
-    distance:number;
-    size:number;
-    displayTip:boolean
+    name: string;
+    date: string;
+    distance: number;
+    size: number;
+    displayTip: boolean
 }
-export const CardContent =(props:AsteroidCardProps) => {
-    const {name,date,distance,size,displayTip}=props;
+export const CardContent = (props: AsteroidCardProps) => {
+    const {name, date, distance, size, displayTip} = props;
     return <div className={"info"}>
         <div><h2><u>{name}</u></h2>
-        {`Дата: ${date}`}<br/>
-        {`Расстояние: ${distanceDisplay(displayTip,distance)}`}<br/>
-        {`Размер: ${size} м`}</div>
+            {`Дата: ${date}`}<br/>
+            {`Расстояние: ${distanceDisplay(displayTip, distance)}`}<br/>
+            {`Размер: ${size} м`}</div>
     </div>
 }
 
-const distanceDisplay = (displayTip,distance) => {
+const distanceDisplay = (displayTip, distance) => {
     if (displayTip)
         return `${distance} км`
     else
-        return `${(distance/384400).toFixed(2)} расстояний до луны`
+        return `${(distance / 384400).toFixed(2)} расстояний до луны`
 }
